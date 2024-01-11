4dbec157edf97fef223eb1e91e2b968d5ae3a982
Om Ganjale <ganjaleom14@gmail.com>
2024-01-11 11:46:25 +0530

    git upload

--- all-react-projects/4-food-item-list/lern-fragment/src/component/ErrorMsg.jsx
+++ all-react-projects/4-food-item-list/lern-fragment/src/component/ErrorMsg.jsx
@@ -1,7 +1,7 @@
 let ErrorMsg = ({items}) => {
 return (
     <>
-      {items.length === 0 ? <h3>I am still hungry</h3> : null}
+      {items.length === 0 ? <center><h3>I am still hungry</h3></center> : null}
     </>
   );
 };
